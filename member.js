fd47181af78e02811017fa3cdb8128209b98b942
mwbaj <149197527+mwbaj@users.noreply.github.com>
2023-11-05 13:14:41 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,35 @@
+function skillsMember() {
+    var skills = document.getElementById("skills");
+    var member = document.getElementById("member");
+    var memberList = document.getElementById("memberList");
+    var skillsList = document.getElementById("skillsList");
+    var memberList = document.getElementById("memberList");
+    var skillsList = document.getElementById("skillsList");
+    var memberList = document.getElementById("memberList");
+    var skillsList = document.getElementById("skillsList");
+    var memberList = document.getElementById("memberList");
+    var skillsList = document.getElementById("skillsList");
+    var memberList = document.getElementById("memberList");
+    var skillsList = document.getElementById("skillsList");
+    var memberList = document.getElementById("memberList");
+    var skillsList = document.getElementById("skillsList");
+    var memberList = document.getElementById("memberList");
+    var skillsList = document.getElementById("skillsList");
+    var memberList = document.getElementById("memberList");
+    var skillsList = document.getElementById("skillsList");
+    var memberList = document.getElementById("memberList");
+    var skillsList = document.getElementById("skillsList");
+    var memberList = document.getElementById("memberList");
+    var skillsList = document.getElementById("skillsList");
+    if (member.style.display === "none") {
+        member.style.display = "block";
+        skills.style.display = "none";
+        memberList.style.display = "block";
+        skillsList.style.display = "none";
+    } else {
+        member.style.display = "none";
+        skills.style.display = "block";
+        memberList.style.display = "none";
+        skillsList.style.display = "block";
+    }
+}
